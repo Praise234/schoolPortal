272cdfe36fe1797bb44aba7018767caee13c69ff
Praise234 <praise207@yahoo.com>
2022-01-26 18:12:46 +0100

    1/26/22 last

--- index.php
+++ index.php
@@ -1,3 +1,43 @@
-<?php
-echo "Hello world";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Vintage Model School</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent fixed-top">
+        <div class="container-fluid">
+            <a href="#" class="navbar-brand">
+                <img src="assets/images/logo.png" alt="logo" class="img-responsive">
+            </a>
+        </div>
+    </nav>
+    <header>
+        <div id="homeCarousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+            <ol class="carousel-indicators">
+                <li data-bs-target="#homeCarousel" data-bs-slide-to="0" class="active"></li>
+                <li data-bs-target="#homeCarousel" data-bs-slide-to="1"></li>
+                <li data-bs-target="#homeCarousel" data-bs-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="d-block w-100" src="assets/images/first.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="assets/images/second.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                <img class="d-block w-100" src="assets/images/third.jpg" alt="Third slide">
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"></script>
+</html>
